d87c66b7dbb1547f00f9d02e84bcd0c6185aa869
Alan D. E. Cruse <alancruse@tutanota.com>
2023-02-14 15:25:29 +0100

    Change css borders when card is closed

--- src/components/CardItem.vue
+++ src/components/CardItem.vue
@@ -1,6 +1,10 @@
 <template>
   <section class="card">
-    <header @click="toggleAccordion()" class="card-header">
+    <header
+      @click="toggleAccordion()"
+      class="card-header"
+      :class="{ open: isOpen }"
+    >
       <i>
         <slot name="icon"></slot>
       </i>
@@ -46,14 +50,23 @@ export default {
   flex-direction: row;
   justify-content: flex-start;
   padding: 8px 16px;
-  border-top-left-radius: 5px;
-  border-top-right-radius: 5px;
-  border-bottom-style: solid;
-  border-bottom-width: 1px;
+  border-top-left-radius: 6px;
+  border-top-right-radius: 6px;
+  border-bottom-left-radius: 6px;
+  border-bottom-right-radius: 6px;
   border-color: var(--color-border);
   background-color: var(--color-background-soft);
   color: #0ac8ff;
   cursor: pointer;
+}
+
+.open {
+  border-top-left-radius: 6px;
+  border-top-right-radius: 6px;
+  border-bottom-left-radius: 0px;
+  border-bottom-right-radius: 0px;
+  border-bottom-style: solid;
+  border-bottom-width: 1px;
 }
 
 .card-body {
@@ -66,8 +79,11 @@ export default {
   padding: 8px 16px;
   border-top-style: solid;
   border-top-width: 1px;
+  border-bottom-left-radius: 6px;
+  border-bottom-right-radius: 6px;
   border-color: var(--color-border);
   background-color: var(--color-background-soft);
+  color: var(--vt-c-text-dark-2);
 }
 
 /* .show-btn {
@@ -78,64 +94,3 @@ export default {
   cursor: pointer;
 } */
 </style>
-
-<!-- <script lang="ts">
-export default {
-  props: ["title"],
-  data() {
-    return {
-      show: true,
-    };
-  },
-};
-</script>
-
-<template>
-  <section class="card">
-    <header class="card-header">
-      <i>
-        <slot name="icon"></slot>
-      </i>
-      <h2>
-        {{ title }}
-      </h2>
-      <button @click="show = !show" class="show-btn">Toggle</button>
-    </header>
-    <div v-if="show" class="card-body">
-      <slot name="contents"></slot>
-    </div>
-    <footer class="card-footer">Credit to <slot name="author"></slot></footer>
-  </section>
-</template>
-
-<style scoped>
-.card {
-  margin-bottom: 40px;
-  border-radius: 6px;
-  background-color: #141e32;
-  grid-column: 1;
-}
-.card-header {
-  color: #0ac8ff;
-  display: flex;
-  flex-direction: row;
-  justify-content: flex-start;
-  padding: 8px 16px;
-  border-top-left-radius: 5px;
-  border-top-right-radius: 5px;
-  border-bottom-style: solid;
-  border-bottom-width: 1px;
-}
-
-.card-body {
-  color: white;
-  padding: 16px;
-}
-
-.card-footer {
-  padding: 8px 16px;
-}
-.show-btn {
-  margin-left: 2px;
-}
-</style> -->
