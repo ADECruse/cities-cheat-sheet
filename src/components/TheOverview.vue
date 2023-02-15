cf219502f845f997be24c5bf15ace63c6fe58f19
Alan D. E. Cruse <alancruse@tutanota.com>
2023-02-15 13:44:45 +0100

    Create TheOverview.vue and add basic content and styling

---
+++ src/components/TheOverview.vue
@@ -0,0 +1,30 @@
+<script lang="ts">
+export default {};
+</script>
+
+<template>
+  <section class="overview">
+    <header>
+      <h2>Overview</h2>
+      <hr />
+    </header>
+
+    <p>Cities: Skylines is a city building simulation game.</p>
+    <p>
+      You are the mayor with the freedom to design and grow your city as you
+      desire. But you must also balance the city budget and maintain good
+      traffic flow.
+    </p>
+  </section>
+</template>
+
+<style scoped>
+.overview {
+  grid-row-start: 2;
+  grid-row-end: 2;
+  padding: 16px;
+}
+hr {
+  margin: 10px 0px;
+}
+</style>
